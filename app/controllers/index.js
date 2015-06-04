02c24ea5641ccdc62e8f3787ed91c1790b6a87f5
GitHubTai <tai@telenet.be>
2015-06-04 16:02:46 +0200

    added switch structure on TableViewRow

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -13,4 +13,37 @@ function outputState(e) {
 	alert('Switch Value: ' + e.value);
 }
 
+
+function tableCheck(e) {
+	
+	// either by doing this with a SWITCH structure
+	switch(e.index) {
+		case 0: 
+			alert("You clicked Apple");
+			break;
+		case 1:
+			alert("You clicked Bananas");
+			break;
+		case 2:
+			alert("You clicked Carrots");
+			break;
+		case 3:
+			alert("You clicked Potatoes");
+			break;
+	}
+	/*
+	
+	// or via a IF else if structure
+	if(e.index == 0) {
+		alert("You clicked on Apple");
+	} else if(e.index == 1){
+		alert("You clicked Bananas");
+	} else if(e.index == 2) {
+		alert("You clicked Carrots");
+	} else if(e.index == 3) {
+		alert("You clicked Potatoes");
+	}
+	*/
+}
+
 $.index.open();
