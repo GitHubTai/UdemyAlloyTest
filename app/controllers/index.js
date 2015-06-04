4c6020a68f0a942d6016c272187594a7cef84361
GitHubTai <tai@telenet.be>
2015-06-04 15:23:29 +0200

    first commit

---
+++ app/controllers/index.js
@@ -0,0 +1,16 @@
+function doClick(e) {  
+    
+    alert("Hi Tai!");
+}
+
+function buttonClick(e) {
+	
+	alert("United on Top");
+}
+
+function outputState(e) {
+	
+	alert('Switch Value: ' + e.value);
+}
+
+$.index.open();
